e481c202ab16d29b65feccab2eca1496801eed9a
SergeyN <yardox@gmail.com>
2022-04-16 10:24:58 +0400

    14 chapter exception

---
+++ testException.py
@@ -0,0 +1,10 @@
+# x = int(input('x: '))
+# y = int(input('y: '))
+# try:
+#     print(x/y)
+# except ZeroDivisionError as var1:
+#     print('Exception div')
+#     print(var1)
+
+x = int(input('x: '))
+assert x != 0, 'not zero'
